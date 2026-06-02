86f054cfd1807047560d03f3dcfa2754aeb21778
keyntk78 <key.ntk@gmail.com>
2026-06-02 23:23:30 +0700

    feat(api): add TokenModule to ApiModule imports

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -3,11 +3,12 @@ import { JwtAuthGuard } from '@common/guards/jwt-auth.guard';
 import { ResponseInterceptor } from '@common/interceptors/response.interceptor';
 import { LoggerService } from '@common/services/logger.service';
 import { ResponseService } from '@common/services/response.service';
+import { TokenModule } from '@infrastructure/token/token.module';
 import { Module } from '@nestjs/common';
 import { ApplicationModule } from '@application/application.module';
 
 @Module({
-  imports: [ApplicationModule],
+  imports: [ApplicationModule, TokenModule],
   controllers: [AuthController],
   providers: [
     LoggerService,
